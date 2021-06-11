264ac7b5911e836fedeb1574d09462c7c110281b
finned-palmer <finnedpalmergithub@worldclean.anonaddy.com>
2021-06-11 14:26:56 -0500

    Convert DateCircle to function

--- interface/src/components/lib/DateCircle.tsx
+++ interface/src/components/lib/DateCircle.tsx
@@ -15,50 +15,51 @@ interface Props {
   month: number
 }
 
-export default class DateCircle extends Component<Props> {
-  constructor(props) {
-    super(props)
+const DateCircle: React.FC<Props> = ({
+  selectDay,
+  sidebar,
+  shaded,
+  day,
+  selectedDay,
+  month,
+  children,
+}) => {
+  const isSameMonth = day?.getMonth() === month
+  const isToday = !!day && isSameMonth && moment(day).isSame(moment(), "day")
+  const isSelected =
+    !!day &&
+    isSameMonth &&
+    !!selectedDay &&
+    moment(day).isSame(moment(selectedDay), "day") &&
+    selectedDay.getMonth() === month
+  let className = "date-circle"
+  let onClick
+  if (selectDay) {
+    className += " clickable"
+    onClick = selectDay(day, sidebar)
+  }
+  if (isToday) {
+    className += " today"
+  }
+  if (isSelected) {
+    className += " selected"
   }
 
-  render() {
-    const {
-      props: { selectDay, day, selectedDay, month, sidebar },
-    } = this
-    const isSameMonth = day?.getMonth() === month
-    const isToday = !!day && isSameMonth && moment(day).isSame(moment(), "day")
-    const isSelected =
-      !!day &&
-      isSameMonth &&
-      !!selectedDay &&
-      moment(day).isSame(moment(selectedDay), "day") &&
-      selectedDay.getMonth() === month
-    let className = "date-circle"
-    let onClick
-    if (selectDay) {
-      className += " clickable"
-      onClick = selectDay(day, sidebar)
-    }
-    if (isToday) {
-      className += " today"
-    }
-    if (isSelected) {
-      className += " selected"
-    }
-
-    return (
-      <Text
-        className={className}
-        onClick={onClick}
-        padding="5px 1px"
-        width="24px"
-        margin="1px 4px"
-        borderRadius="13px"
-        textAlign="center"
-        verticalAlign="middle"
-        opacity={!!this.props.shaded ? "0.5" : "0.9"}
-      >
-        {this.props.children}
-      </Text>
-    )
-  }
+  return (
+    <Text
+      className={className}
+      onClick={onClick}
+      padding="5px 1px"
+      width="24px"
+      margin="1px 4px"
+      borderRadius="13px"
+      textAlign="center"
+      verticalAlign="middle"
+      opacity={!!shaded ? "0.5" : "0.9"}
+    >
+      {children}
+    </Text>
+  )
 }
+
+export default DateCircle
