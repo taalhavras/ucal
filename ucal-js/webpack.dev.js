2024369a05d48bbfdf2c5ec6489032e625a593b5
finned-palmer <finned-palmer@togten.com>
2021-10-23 14:36:15 -0500

    Update webpack dev config to match btc-wallet

--- ucal-js/webpack.dev.js
+++ ucal-js/webpack.dev.js
@@ -1,12 +1,14 @@
-const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const urbitrc = require('./.urbitrc');
-const fs = require('fs-extra');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const urbitrc = require("./.urbitrc");
+const fs = require("fs-extra");
+const _ = require("lodash");
 
-function copy(src,dest) {
-  return new Promise((res,rej) =>
-    fs.copy(src,dest, err => err ? rej(err) : res()));
+function copy(src, dest) {
+  return new Promise((res, rej) =>
+    fs.copy(src, dest, (err) => (err ? rej(err) : res()))
+  );
 }
 
 class UrbitShipPlugin {
@@ -16,103 +18,120 @@ class UrbitShipPlugin {
 
   apply(compiler) {
     compiler.hooks.afterEmit.tapPromise(
-      'UrbitShipPlugin',
+      "UrbitShipPlugin",
       async (compilation) => {
-        const src = './app'
+        // const src = "./src/index.js";
 
-        return Promise.all(this.piers.map(pier => {
-          const dst = path.resolve(pier, 'app');
-          copy(src, dst).then(() => {
-            pier = pier.split('/');
-          });
-        }));
+        const src = path.resolve(compiler.options.output.path, "index.js");
       }
     );
   }
 }
 
 let devServer = {
-  contentBase: path.resolve('./app/calendar/js'),
+  contentBase: path.resolve(__dirname, "./dist"),
   hot: true,
   port: 9000,
+  host: "0.0.0.0",
+  disableHostCheck: true,
   historyApiFallback: true,
-  writeToDisk: (filePath) => {
-    return /index.js$/.test(filePath);
-  }
+  publicPath: "/apps/calendar",
+  // writeToDisk: (filePath) => {
+  // return /index.js$/.test(filePath);
+  // },
 };
 
-if(urbitrc.URL) {
+const router = _.mapKeys(
+  urbitrc.FLEET || {},
+  (value, key) => `${key}.localhost:9000`
+);
+
+if (urbitrc.URL) {
   devServer = {
     ...devServer,
-    index: '',
-    proxy: {
-      '/~calendar/js/index.js': {
-        target: 'http://localhost:9000',
-        pathRewrite: (req, path) => '/index.js'
-      },
-      '**': {
+    index: "index.html",
+    proxy: [
+      {
+        // target: "http://localhost:9000",
+        changeOrigin: true,
         target: urbitrc.URL,
-        // ensure proxy doesn't timeout channels
-        proxyTimeout: 0
-      }
-    }
+        router,
+        context: (path) => {
+          if (path === "/apps/calendar/desk.js") {
+            return true;
+          }
+          return !path.startsWith("/apps/calendar");
+        },
+      },
+    ],
   };
 }
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    app: './interface/src/index.js'
-  },
+  node: { fs: "empty" },
+  mode: "development",
+  entry: "./src/index.js",
   module: {
     rules: [
       {
         test: /\.(j|t)sx?$/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-typescript",
+              "@babel/preset-react",
+            ],
             plugins: [
-              '@babel/transform-runtime',
-              '@babel/plugin-proposal-object-rest-spread',
-              '@babel/plugin-proposal-optional-chaining',
-              '@babel/plugin-proposal-class-properties',
-              'react-hot-loader/babel'
-            ]
-          }
+              "@babel/transform-runtime",
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-proposal-optional-chaining",
+              "@babel/plugin-proposal-class-properties",
+              "react-hot-loader/babel",
+            ],
+          },
         },
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader'
-        ]
-      }
-    ]
+          "sass-loader",
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+    extensions: [".js", ".ts", ".tsx"],
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: devServer,
   plugins: [
-    new UrbitShipPlugin(urbitrc)
+    new UrbitShipPlugin(urbitrc),
+    new HtmlWebpackPlugin({
+      title: "UCal",
+      template: "./public/index.html",
+    }),
   ],
   watch: true,
+  watchOptions: {
+    poll: true,
+    ignored: "/node_modules/",
+  },
   output: {
-    filename: 'index.js',
-    chunkFilename: 'index.js',
-    path: path.resolve('./app/calendar/js'),
-    publicPath: '/'
+    filename: "index.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/apps/calendar/",
+    globalObject: "this",
   },
   optimization: {
     minimize: false,
-    usedExports: true
-  }
+    usedExports: true,
+  },
 };
