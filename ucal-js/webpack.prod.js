cd8734f206c4b499b4477ed640c0f7c594b1b441
raghu <{ID}+{username}@users.noreply.github.com>
2021-12-20 23:57:21 -0500

    update webpack so that 'yarn build' will work

--- ucal-js/webpack.prod.js
+++ ucal-js/webpack.prod.js
@@ -1,49 +1,53 @@
-const path = require('path');
+const path = require("path");
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-     app: './interface/src/index.js'
+    app: "./src/index.js",
   },
   module: {
     rules: [
       {
         test: /\.(j|t)sx?$/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/typescript', '@babel/preset-react'],
+            presets: [
+              "@babel/preset-env",
+              "@babel/typescript",
+              "@babel/preset-react",
+            ],
             plugins: [
-              'lodash',
-              '@babel/transform-runtime',
-              '@babel/plugin-proposal-object-rest-spread',
-              '@babel/plugin-proposal-optional-chaining',
-              '@babel/plugin-proposal-class-properties'
-            ]
-          }
+              "lodash",
+              "@babel/transform-runtime",
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-proposal-optional-chaining",
+              "@babel/plugin-proposal-class-properties",
+            ],
+          },
         },
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader'
-        ]
-      }
-    ]
+          "sass-loader",
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+    extensions: [".js", ".ts", ".tsx"],
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   // devServer: {
   //   contentBase: path.join(__dirname, './'),
   //   hot: true,
@@ -59,12 +63,12 @@ module.exports = {
     // }),
   ],
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, `./urbit/app/calendar/js`),
-    publicPath: '/',
+    filename: "index.js",
+    path: path.resolve(__dirname, `../ucal/app/calendar/js`),
+    publicPath: "/",
   },
   optimization: {
     minimize: true,
-    usedExports: true
-  }
+    usedExports: true,
+  },
 };
