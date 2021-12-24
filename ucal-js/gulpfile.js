d98a73717f74069d475d13f5a21e676517648c45
raghu <{ID}+{username}@users.noreply.github.com>
2021-12-23 19:26:17 -0500

    undo unecessary changes

--- ucal-js/gulpfile.js
+++ ucal-js/gulpfile.js
@@ -1,26 +1,29 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var print = require("gulp-print").default;
 
-var urbitrc = require('./.urbitrc');
+var urbitrc = require("./.urbitrc");
 
-gulp.task('urbit-copy', function () {
-  let ret = gulp.src('urbit/**/*');
+gulp.task("urbit-copy", function (done) {
+  let ret = gulp.src("../ucal/*");
 
-  urbitrc.URBIT_PIERS.forEach(function(pier) {
-    ret = ret.pipe(gulp.dest(pier));
+  urbitrc.URBIT_PIERS.forEach(function (pier) {
+    return ret.pipe(gulp.dest(pier)).pipe(
+      print(function () {
+        return "Copied to: " + pier;
+      })
+    );
   });
 
-  return ret;
+  done();
 });
 
+gulp.task("bundle-dev", gulp.series("urbit-copy"));
+
+gulp.task("default", gulp.series("bundle-dev"));
+
 gulp.task(
-  'bundle-dev',
-  gulp.series(
-    'urbit-copy'
-  )
+  "watch",
+  gulp.series("default", function () {
+    gulp.watch("../ucal/*", gulp.parallel("urbit-copy"));
+  })
 );
-
-gulp.task('default', gulp.series('bundle-dev'));
-
-gulp.task('watch', gulp.series('default', function() {
-  gulp.watch('urbit/**/*', gulp.parallel('urbit-copy'));
-}));
